04a173888940b9d797666bbbd8ab351dafede853
Nathan Woodward <nathanrichardwoodward@gmail.com>
2019-12-31 13:12:50 +0000

    thought soe more

--- guilib.h
+++ guilib.h
@@ -16,6 +16,13 @@ typedef const char* utf8_string;
 // Widget identifier. 0 always identifies the root widget, which always exists and cannot be destroyed.
 typedef uint32_t widget_id;
 
+typedef uint32_t action_id;
+
+enum widget_type {
+    window = 0,
+    button = 1
+};
+
 
 //----- Create and configure widgets
 
@@ -31,18 +38,25 @@ error create_widgets_from_text(struct ctx*, widget_id parent, utf8_string text);
 // Create a widget.
 //
 // If a widget with the same name already exists, the name is disambiguated with a number.
-error create_widget(struct ctx*, utf8_string name, widget_id parent, widget_id* widget_id_out);
+//
+// 'parent' must be a widget with a 'client area'.  The new widget will be displayed in
+// the 'client area' of the parent widget.
+//
+// the newly created widget may have any number of automatically created child widgets in 
+// its 'non client area' - e.g. window frame buttons.
+error create_widget(
+  struct ctx*, 
+  widget_type type, 
+  utf8_string name, 
+  widget_id parent, 
+  widget_id* widget_id_out
+);
 
 // Get a list of child widgets.
 error get_children(struct ctx*, widget_id, widget_id* buf, uint64_t buf_size, uint64_t* size_needed_out);
 
 // Get/set properties.
-error set_property_string(struct ctx*, widget_id widget, utf8_string key, utf8_string value);
-error set_property_real(struct ctx*, widget_id widget, utf8_string key, float value);
-error set_property_int(struct ctx*, widget_id widget, utf8_string key, uint32_t value);
-error get_property_string(struct ctx*, widget_id widget, utf8_string key, char* buf, uint64_t buf_size, uint64_t* size_needed_out);
-error get_property_real(struct ctx*, widget_id widget, utf8_string key, float* value_out);
-error get_property_int(struct ctx* widget_id widget, utf8_string key, uint32_t* value_out);
+error get_properties(struct ctx*, widget_id widget, struct data_node* out);
 
 
 //----- Input
@@ -51,12 +65,57 @@ error get_property_int(struct ctx* widget_id widget, utf8_string key, uint32_t* 
 // and return any requested actions.  Need to support arbitrary input devices.
 error input(struct ctx*);
 
+struct command {
+    widget_id widget;
+    action_id action; // register action ids via api? ... or just ask widget for its action code?
+};
+
 
 //----- Update
 
 // The UI doesn't have its own main loop, so the application must inform it of 
 // the passage of time.  This is used to update animations and so on.
 error update(float dt);
+
+// how to associate a widget with a value in the program?
+
+// label foo {
+//   expr: "bar.qux"
+// }
+
+// how would the above expr be evaluated?   inject an interface?   some api like:
+
+
+struct data_node;
+struct compiled_expr;
+
+// expr factory func
+typedef error compile_expr_func(utf8_string expr, struct compiled_expr* expr);
+
+// expr class
+typedef error evaluate_expr_func(struct compiled_expr* expr, struct data_node* out); // called when updating widget
+typedef error free_compiled_expr_func(struct compiled_expr*);
+struct compiled_expr {
+    evaluate_expr_func evaluate;
+    free_compiled_expr_func free;
+    void* handle;
+};
+
+// data node class
+typedef error get_child_node_by_name(struct data_node* node, utf8_string key, struct data_node* out);
+// get_child_node_by_index
+// get_value_string
+// get_value_int
+// get_value_real
+// set_value_string
+// set_value_int
+// set_value_real
+// get_num_children
+// free_data_node
+struct data_node {
+  // ...    
+  void* handle; 
+};
 
 
 //----- Draw
