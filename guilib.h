5a1f19ea76c15bdb91d6daa135b2745b74ee5dc1
Nathan Woodward <nathanrichardwoodward@gmail.com>
2020-01-02 07:16:49 +0000

    thought

--- guilib.h
+++ guilib.h
@@ -8,20 +8,54 @@
 //   - abstract support for sounds (inject, or return...)
 //   - renderer agnostic - but try to do as much work as possible
 
-struct ctx {};
+struct nui_gui;
+struct nui_data;
 
-typedef uint32_t error;
-typedef const char* utf8_string;
+typedef uint32_t nui_unsigned;
+typedef int32_t nui_int;
+typedef uint32_t nui_error;
+typedef const char* nui_utf8;
+typedef uint64_t nui_size;
+typedef float nui_real;
 
 // Widget identifier. 0 always identifies the root widget, which always exists and cannot be destroyed.
-typedef uint32_t widget_id;
+typedef uint32_t nui_widget_id;
 
-typedef uint32_t action_id;
-
-enum widget_type {
-    window = 0,
-    button = 1
+enum nui_widget_type {
+    NUI_WIDGET_TYPE_WINDOW = 0,
+    NUI_WIDGET_TYPE_BUTTON
 };
+
+enum nui_error {
+    NUI_ERROR_OK = 0,
+    NUI_ERROR_UNKNOWN
+}
+
+
+//----- Data node interface
+
+struct nui_data;
+typedef enum nui_error nui_data_get_child_node_by_name_func(
+    struct nui_data* node, 
+    nui_utf8 key, 
+    struct nui_data* out
+);
+// get_child_node_by_index
+// get_value_string
+// get_value_int
+// get_value_real
+// set_value_string
+// set_value_int
+// set_value_real
+// get_num_children
+// acquire_reference
+// release_reference
+struct nui_data {
+  nui_data_get_child_node_by_name_func get_child_node_by_name;
+  // ...
+  void* handle; 
+};
+
 
 
 //----- Create and configure widgets
@@ -30,10 +64,18 @@ enum widget_type {
 // set the 'visible' property to TRUE.
 
 // Create widgets from text from a file
-error create_widgets_from_file(struct ctx*, widget_id parent, utf8_string filename);
+enum nui_error nui_create_widgets_from_file(
+    struct nui_gui*, 
+    nui_widget_id parent, 
+    nui_utf8 filename
+);
 
 // Create widgets from text
-error create_widgets_from_text(struct ctx*, widget_id parent, utf8_string text);
+enum nui_error nui_create_widgets_from_text(
+    struct nui_gui*, 
+    nui_widget_id parent, 
+    nui_utf8 text
+);
 
 // Create a widget.
 //
@@ -44,30 +86,48 @@ error create_widgets_from_text(struct ctx*, widget_id parent, utf8_string text);
 //
 // the newly created widget may have any number of automatically created child widgets in 
 // its 'non client area' - e.g. window frame buttons.
-error create_widget(
-  struct ctx*, 
-  widget_type type, 
-  utf8_string name, 
-  widget_id parent, 
-  widget_id* widget_id_out
+enum nui_error nui_create_widget(
+    struct nui_gui*, 
+    enum nui_widget_type type, 
+    nui_utf8 name, 
+    nui_widget_id parent, 
+    nui_widget_id* widget_id_out
 );
 
 // Get a list of child widgets.
-error get_children(struct ctx*, widget_id, widget_id* buf, uint64_t buf_size, uint64_t* size_needed_out);
+enum nui_error nui_get_widget_children(
+    struct nui_gui*, 
+    enum nui_widget_id, 
+    enum nui_widget_id* buf, 
+    nui_size buf_size, 
+    nui_size* size_needed_out
+);
+
+// Get a named widget.  The name can use 'dot' notation to navigate
+// the hierarchy.
+enum nui_error nui_get_widget(
+    struct nui_gui*,
+    nui_widget_id parent,
+    nui_utf8 name,
+    nui_widget_id* widget_id_out
+);
 
 // Get/set properties.
-error get_properties(struct ctx*, widget_id widget, struct data_node* out);
+enum nui_error nui_get_widget_properties(
+    struct nui_gui*, 
+    nui_widget_id widget, 
+    struct nui_data* out
+);
 
 
 //----- Input
 
 // Note: need to flesh this out.  Need to pass input from the application into the library,
 // and return any requested actions.  Need to support arbitrary input devices.
-error input(struct ctx*);
+enum nui_error nui_input(struct nui_gui*);
 
-struct command {
-    widget_id widget;
-    action_id action; // register action ids via api? ... or just ask widget for its action code?
+struct nui_command {
+    nui_widget_id widget;
 };
 
 
@@ -75,7 +135,7 @@ struct command {
 
 // The UI doesn't have its own main loop, so the application must inform it of 
 // the passage of time.  This is used to update animations and so on.
-error update(float dt);
+enum nui_error nui_update(struct nui_gui*, nui_real dt);
 
 // how to associate a widget with a value in the program?
 
@@ -85,41 +145,28 @@ error update(float dt);
 
 // how would the above expr be evaluated?   inject an interface?   some api like:
 
-
-struct data_node;
-struct compiled_expr;
+struct nui_compiled_expr;
 
 // expr factory func
-typedef error compile_expr_func(utf8_string expr, struct compiled_expr* expr);
+typedef enum nui_error nui_compile_expr_func(nui_utf8 expr, struct nui_compiled_expr* out);
 
 // expr class
-typedef error evaluate_expr_func(struct compiled_expr* expr, struct data_node* out); // called when updating widget
-typedef error free_compiled_expr_func(struct compiled_expr*);
+typedef enum nui_error evaluate_expr_func(
+    struct nui_compiled_expr* expr, 
+    struct nui_data* out
+);
+// acquire_reference
+// release_reference
 struct compiled_expr {
     evaluate_expr_func evaluate;
-    free_compiled_expr_func free;
+    // ...
     void* handle;
 };
 
-// data node class
-typedef error get_child_node_by_name(struct data_node* node, utf8_string key, struct data_node* out);
-// get_child_node_by_index
-// get_value_string
-// get_value_int
-// get_value_real
-// set_value_string
-// set_value_int
-// set_value_real
-// get_num_children
-// free_data_node
-struct data_node {
-  // ...    
-  void* handle; 
-};
 
 
 //----- Draw
 
 // This needs to output either a sequence of drawing commands as per nuklear, or something more abstract
 // to allow for greater customisation of what is displayed by the application
-error draw(struct ctx*);
+enum nui_error nui_draw(struct nui_gui*);
